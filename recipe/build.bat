5dac5dea311b4aa2eab66e4a327bc3d87b9a1ce6
Pavel Zwerschke <pavelzw@gmail.com>
2026-09-10 11:05:03 +0200

    set global-ignore-conda-prefix marker (#4)

--- recipe/build.bat
+++ recipe/build.bat
@@ -7,3 +7,10 @@ call pnpm install --prod --ignore-scripts
 if errorlevel 1 exit /b 1
 call pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 if errorlevel 1 exit /b 1
+
+REM Pixi: prevent CONDA_PREFIX from leaking into sandboxed processes
+set "MARKER_DIR=%PREFIX%\etc\pixi\diffle"
+if not exist "%MARKER_DIR%" (
+    mkdir "%MARKER_DIR%" 2>nul
+)
+type nul > "%MARKER_DIR%\global-ignore-conda-prefix"
