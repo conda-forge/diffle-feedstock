1f89f84503adcb6f4293172e246d50e6b60eb19f
Pavel Zwerschke <pavelzw@gmail.com>
2026-09-10 10:45:20 +0200

    set global-ignore-conda-prefix marker

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,3 +6,7 @@ npm install --global --prefix "${PREFIX}" --ignore-scripts --omit=dev --install-
 export PNPM_CONFIG_PM_ON_FAIL=ignore
 pnpm install --prod --ignore-scripts
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+# Pixi: prevent CONDA_PREFIX from leaking into sandboxed processes
+mkdir -p "${PREFIX}/etc/pixi/diffle"
+touch "${PREFIX}/etc/pixi/diffle/global-ignore-conda-prefix"
